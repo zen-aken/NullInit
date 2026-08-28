aca933e000795b1db52c89e1ca675f9e73a53128
zen-aken <arslanahmet4772@gmail.com>
2026-08-28 22:38:00 +0200

    - Link stdin/out/err

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,20 @@ mod shell;
 
 fn main() {
     unsafe {
+        // file system
         mount::mount_fs("proc", "/proc", "proc", 0);
         mount::mount_fs("sysfs", "/sys", "sysfs", 0);
         mount::mount_fs("devtmpfs", "/dev", "devtmpfs", 0);
 
+        // console
+        let console = std::ffi::CString::new("/dev/console").unwrap();
+        let fd = libc::open(console.as_ptr(), libc::O_RDWR);
+
+        libc::dup2(fd, 0); // stdin
+        libc::dup2(fd, 1); // stdout
+        libc::dup2(fd, 2); // stderr
+
+        // shell
         shell::spawn();
     }
 
